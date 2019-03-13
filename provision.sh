e4857feff57cfd2a87fc043cdacdb1ba6e67c65a
Jan Tomický <jan@tomicky.cz>
2019-03-13 09:27:36 +0100

    added php bz2 extension installation to the provisioning script

--- provision.sh
+++ provision.sh
@@ -75,7 +75,7 @@ apt-get install -y php7.2 php7.2-fpm
 
 # Install the required PHP modules.
 echo "Installing PHP modules…"
-apt-get install -y php7.2-mysql php7.2-xml php7.2-curl php7.2-gd php7.2-mbstring php7.2-zip php7.2-intl php7.2-dev
+apt-get install -y php7.2-mysql php7.2-xml php7.2-curl php7.2-gd php7.2-bz2 php7.2-mbstring php7.2-zip php7.2-intl php7.2-dev
 
 # Install PEAR.
 echo "Installing PEAR…"
