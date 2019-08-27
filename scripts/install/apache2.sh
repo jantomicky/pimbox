ef4157edf58351323ddcac9c197adfd106c5622b
Jan Tomický <jan@tomicky.cz>
2019-08-27 11:17:44 +0200

    set explicit apach2 umask

--- scripts/install/apache2.sh
+++ scripts/install/apache2.sh
@@ -20,8 +20,8 @@ adduser vagrant www-data
 echo "Adding 'www-data' user to the 'vagrant' group…"
 adduser www-data vagrant
 
-echo "Setting 'www-data' user umask (002) to grant group write permissions…"
-echo "umask 002" >> /etc/apache2/envvars
+echo "Setting 'www-data' user umask (0002) to grant group write permissions…"
+echo "umask 0002" >> /etc/apache2/envvars
 
 echo "Restarting the apache2 service…"
 systemctl restart apache2.service
