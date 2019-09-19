ee47c857fc1b5702d49a5ae5333f00417d494e55
Jan Tomický <jan@tomicky.cz>
2019-09-19 10:20:50 +0200

    improved PHP configuration

--- scripts/install/php7.0.sh
+++ scripts/install/php7.0.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-CONFIGURATION_PHP="/etc/php/7.0/cli/php.ini /etc/php/7.0/fpm/php.ini"
+CONFIG_PHP_FPM="/etc/php/7.0/fpm/php.ini"
+CONFIG_PHP_CLI="/etc/php/7.0/cli/php.ini"
 
 echo "Installing PHP 7.0…"
 apt-get install -y php7.0 php7.0-fpm
@@ -9,11 +10,12 @@ echo "Installing PHP modules…"
 apt-get install -y php7.0-mysql php7.0-xml php7.0-curl php7.0-soap php7.0-gd php7.0-bz2 php7.0-mbstring php7.0-zip php7.0-intl php7.0-dev php7.0-xdebug
 
 echo "Configuring PHP…"
-sed -i "s|;session.save_path|session.save_path|" $CONFIGURATION_PHP
-sed -i "s|short_open_tag = Off|short_open_tag = On|" $CONFIGURATION_PHP
-sed -i "s|memory_limit = 128M|memory_limit = 1024M|" $CONFIGURATION_PHP
-sed -i "s|upload_max_filesize = 2M|upload_max_filesize = 128M|" $CONFIGURATION_PHP
-sed -i "s|post_max_size = 8M|post_max_size = 128M|" $CONFIGURATION_PHP
+sed -i "s|short_open_tag = Off|short_open_tag = On|" $CONFIG_PHP_FPM $CONFIG_PHP_CLI
+sed -i "s|memory_limit = 128M|memory_limit = 1024M|" $CONFIG_PHP_FPM $CONFIG_PHP_CLI
+sed -i "s|;session.save_path|session.save_path|" $CONFIG_PHP_FPM
+sed -i "s|upload_max_filesize = 2M|upload_max_filesize = 128M|" $CONFIG_PHP_FPM
+sed -i "s|post_max_size = 8M|post_max_size = 128M|" $CONFIG_PHP_FPM
+sed -i "s|max_execution_time = 30|max_execution_time = 300|" $CONFIG_PHP_FPM
 
 echo "Configuring XDebug…"
 cat >> /etc/php/7.0/mods-available/xdebug.ini <<EOL
--- scripts/install/php7.2.sh
+++ scripts/install/php7.2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-CONFIGURATION_PHP="/etc/php/7.2/cli/php.ini /etc/php/7.2/fpm/php.ini"
+CONFIG_PHP_FPM="/etc/php/7.2/fpm/php.ini"
+CONFIG_PHP_CLI="/etc/php/7.2/cli/php.ini"
 
 echo "Installing PHP 7.2…"
 apt-get install -y php7.2 php7.2-fpm
@@ -9,11 +10,12 @@ echo "Installing PHP modules…"
 apt-get install -y php7.2-mysql php7.2-xml php7.2-curl php7.2-soap php7.2-gd php7.2-bz2 php7.2-mbstring php7.2-zip php7.2-intl php7.2-dev php7.2-xdebug
 
 echo "Configuring PHP…"
-sed -i "s|;session.save_path|session.save_path|" $CONFIGURATION_PHP
-sed -i "s|short_open_tag = Off|short_open_tag = On|" $CONFIGURATION_PHP
-sed -i "s|memory_limit = 128M|memory_limit = 1024M|" $CONFIGURATION_PHP
-sed -i "s|upload_max_filesize = 2M|upload_max_filesize = 128M|" $CONFIGURATION_PHP
-sed -i "s|post_max_size = 8M|post_max_size = 128M|" $CONFIGURATION_PHP
+sed -i "s|short_open_tag = Off|short_open_tag = On|" $CONFIG_PHP_FPM $CONFIG_PHP_CLI
+sed -i "s|memory_limit = 128M|memory_limit = 1024M|" $CONFIG_PHP_FPM $CONFIG_PHP_CLI
+sed -i "s|;session.save_path|session.save_path|" $CONFIG_PHP_FPM
+sed -i "s|upload_max_filesize = 2M|upload_max_filesize = 128M|" $CONFIG_PHP_FPM
+sed -i "s|post_max_size = 8M|post_max_size = 128M|" $CONFIG_PHP_FPM
+sed -i "s|max_execution_time = 30|max_execution_time = 300|" $CONFIG_PHP_FPM
 
 echo "Configuring XDebug…"
 cat >> /etc/php/7.2/mods-available/xdebug.ini <<EOL
